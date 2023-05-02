ff1f5e2f3ee3d9007bc14872390c9fb47a4125fe
unknown <sagarmikeylevi@gmail.com>
2023-05-02 12:26:38 +0530

    defining routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Home";
+import Products from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Homepage /> },
+  { path: "/products", element: <Products /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+const Products = () => {
+  return <h1>Product page</h1>;
+};
+
+export default Products;
